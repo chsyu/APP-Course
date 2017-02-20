bead26baeff4a9904243a7994461ebd136c83b64
Yu Chi-Shan <chsyu@tea.ntue.edu.tw>
2017-02-20 18:06:52 +0800

    wk1 & wk2

---
+++ wk2/B1_Variables/Starter/app.js
@@ -0,0 +1,42 @@
+// Your Javascript Code Goes Here
+
+// var
+var name = 'ntue';
+var job = 'student';
+// var job = 'teacher';
+// job = 'teacherjob';
+
+// const
+const name1 = 'dtd';
+const job1 = 'student';
+// const job1 = 'teacher';
+// job1 = 'teacher';
+
+// let
+let name2 = 'ntue';
+let job2 = 'student';
+// let job2 = 'teacher';
+// job2 = 'teacherjob';
+
+// var
+console.log(job);
+{
+    console.log(job);
+    var job = 'teachernew';
+    console.log(job);
+}
+console.log(job);
+
+// let
+console.log(job2);
+{
+    console.log(job2);
+    let job2 = 'teacher2new';
+    console.log(job2);
+}
+console.log(job2);
+
+
+var name = "DTD";
+var age = 10;
+console.log("My name is " + name + ", and My age is " + age);
