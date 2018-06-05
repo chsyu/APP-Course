20b2cc8ac0eebe8e727b8c5c3ef6a94eb54d37ec
Yu Chi-Shan <chsyu@tea.ntue.edu.tw>
2018-06-05 12:07:03 +0800

    wkF - Animated Marker

--- wkF/F2_AnimatedMarker/src/components/Metro.js
+++ wkF/F2_AnimatedMarker/src/components/Metro.js
@@ -1,171 +1,234 @@
 import React, { Component } from 'react';
-import { StyleSheet, Platform, View, ActivityIndicator, Animated } from 'react-native';
+import { TouchableWithoutFeedback, StyleSheet, Platform, View, ActivityIndicator, Animated } from 'react-native';
 import { MapView, Permissions, Location, Constants } from 'expo';
 import { Button, Icon } from 'react-native-elements';
 import metro from '../json/metro.json';
 
 class Metro extends Component {
 
-   constructor(props) {
-      super(props);
-      const ballRadius = new Animated.Value(35);
-      const ballWidth = new Animated.Value(70);
+  constructor(props) {
+    super(props);
+    const ballRadius = new Animated.Value(35);
+    const ballWidth = new Animated.Value(70);
+    const markerWidth = new Animated.Value(30);
+    const markerRadius = new Animated.Value(15);
+    const markerOpacity = new Animated.Value(0.7);
 
-      this.state = {
-         mapLoaded: false,
-         region: {
-            longitude: 121.544637,
-            latitude: 25.024624,
-            longitudeDelta: 0.01,
-            latitudeDelta: 0.02
-         },
-         marker: {
-            latlng: {
-               longitude: 121.544637,
-               latitude: 25.024624
-            },
-            description: 'This is NTUE'
-         },
-         metro: [],
-         errorMessage: null,
-         ballRadius,
-         ballWidth
-      }
+    this.state = {
+      mapLoaded: false,
+      region: {
+        longitude: 121.544637,
+        latitude: 25.024624,
+        longitudeDelta: 0.01,
+        latitudeDelta: 0.02
+      },
+      marker: {
+        latlng: {
+          longitude: 121.544637,
+          latitude: 25.024624
+        },
+        description: 'This is NTUE'
+      },
+      metro: [],
+      errorMessage: null,
+      touched: false,
+      siteId: null,
+      markerWidth,
+      markerRadius,
+      markerOpacity
+    }
+  }
 
-   }
 
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Metro',
+      tabBarLabel: 'Metro',
+      tabBarIcon: ({ tintColor }) => <Icon name="add-location" size={35} color={tintColor} />,
+      drawerLabel: 'Metro',
+      drawerIcon: ({ tintColor }) => <Icon name="add-location" size={25} color={tintColor} />,
+      headerLeft: (
+        <Icon
+          name='menu'
+          iconStyle={{ marginLeft: 10 }}
+          onPress={
+            () => navigation.openDrawer()
+          }
+        />
+      ),
+    }
+  };
 
-   static navigationOptions = ({ navigation }) => {
-      return {
-         title: 'Metro',
-         tabBarLabel: 'Metro',
-         tabBarIcon: ({ tintColor }) => <Icon name="add-location" size={35} color={tintColor} />,
-         drawerLabel: 'Metro',
-         drawerIcon: ({ tintColor }) => <Icon name="add-location" size={25} color={tintColor} />,
-         headerLeft: (
-            <Icon
-               name='menu'
-               iconStyle={{ marginLeft: 10 }}
-               onPress = {
-                  () => navigation.openDrawer()
-               }
-            />
-         ),
-      }
-   };
-
-   componentWillMount() {
-      this.setState({ metro });
-      if (Platform.OS === 'android' && !Constants.isDevice) {
-         this.setState({
-            errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-         });
-      } else {
-         this._getLocationAsync();
-      }
-   }
-
-   _getLocationAsync = () => {
-      Permissions.askAsync(Permissions.LOCATION)
-         .then(status => {
-            if (status !== 'granted') {
-               this.setState({
-                  errorMessage: 'Permission to access location was denied',
-               });
-            }
-
-         });
-
-      Location.getCurrentPositionAsync({}).then(location => {
-         console.log(location.coords);
-         // this.setState({
-         //    region: {
-         //       latitude: location.coords.latitude,
-         //       longitude: location.coords.longitude,
-         //       longitudeDelta: 0.01,
-         //       latitudeDelta: 0.02
-         //    }
-         // });
+  componentWillMount() {
+    this.setState({ metro });
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
       });
-   };
+    } else {
+      this._getLocationAsync();
+    }
+  }
 
-   componentDidMount() {
-      this.setState({ mapLoaded: true });
-   }
-
-   onRegionChangeComplete = (region) => {
-      console.log(region);
-      this.setState({ region });
-   }
-
-   render() {
-      if (!this.state.mapLoaded) {
-         return (
-            <View style={{ flex: 1, justifyContent: 'center' }}>
-               <ActivityIndicator size="large" />
-            </View>
-         );
-      }
-
-      console.log(this.state.metro);
-   
-      const { ballRadius, ballWidth } = this.state;
-
+  renderTouchedSite(siteId) {
+    const { markerWidth, markerRadius, markerOpacity } = this.state;
+    if (siteId == this.state.siteId) {
       return (
-         <View style={{ flex: 1 }}>
-            <MapView
-               region={this.state.region}
-               style={{ flex: 1 }}
-               onRegionChangeComplete={this.onRegionChangeComplete}
-            >
-
-               {this.state.metro.map(site => (
-                  <MapView.Marker
-                     coordinate={{ latitude: site.latitude, longitude: site.longitude }}
-                     key={`${site.id}${site.line}`}
-                     title={site.name}
-                     description={site.address}
-                  // image={require('../assets/flag-pink.png')}
-                  >
-                     <Animated.View style={[styles.markerWrap]}>
-                     <Animated.View style={[styles.ring]} />
-                     < View style = {
-                           [styles.marker, 
-                              {
-                                 width: ballWidth,
-                                 height: ballWidth,
-                                 borderRadius: ballRadius,
-                              }
-                            ]} 
-                    />
-                </Animated.View>
-
-                  </MapView.Marker>
-               ))}
-
-            </MapView>
-         </View>
+        <Animated.View
+          style={{
+            width: markerWidth,
+            height: markerWidth,
+            borderRadius: markerRadius,
+            opacity: markerOpacity,
+            backgroundColor: "rgba(130,4,150, 0.9)",
+          }
+          }
+        />
       );
-   }
+    }
+    return (
+      <Animated.View
+        style={{
+          width: 30,
+          height: 30,
+          borderRadius: 15,
+          opacity: 0.7,
+          backgroundColor: "rgba(130,4,150, 0.9)",
+        }
+        }
+      />
+
+    );
+  }
+
+  startAnimation(siteId) {
+    if (!this.state.touched) {
+      this.setState({ touched: true, siteId });
+
+      Animated.parallel([
+        Animated.spring(this.state.markerWidth, {
+          toValue: 50
+        }),
+        Animated.spring(this.state.markerRadius, {
+          toValue: 25
+        }),
+        Animated.spring(this.state.markerOpacity, {
+          toValue: 0.35
+        })
+      ]).start(() => this.startAnimation());
+
+    } else {
+      this.setState({ touched: false });
+      Animated.parallel([
+        Animated.spring(this.state.markerWidth, {
+          toValue: 30
+        }),
+        Animated.spring(this.state.markerRadius, {
+          toValue: 15
+        }),
+        Animated.spring(this.state.markerOpacity, {
+          toValue: 0.7
+        })
+      ]).start();
+    }
+  }
+
+  _getLocationAsync = () => {
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(status => {
+        if (status !== 'granted') {
+          this.setState({
+            errorMessage: 'Permission to access location was denied',
+          });
+        }
+
+      });
+
+    Location.getCurrentPositionAsync({}).then(location => {
+      console.log(location.coords);
+      // this.setState({
+      //    region: {
+      //       latitude: location.coords.latitude,
+      //       longitude: location.coords.longitude,
+      //       longitudeDelta: 0.01,
+      //       latitudeDelta: 0.02
+      //    }
+      // });
+    });
+  };
+
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
+  }
+
+  onRegionChangeComplete = (region) => {
+    console.log(region);
+    this.setState({ region });
+  }
+
+  render() {
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+
+    console.log(this.state.metro);
+
+
+    return (
+      <View style={{ flex: 1 }}>
+        <MapView
+          region={this.state.region}
+          style={{ flex: 1 }}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        >
+
+          {this.state.metro.map(site => (
+            <MapView.Marker
+              coordinate={{ latitude: site.latitude, longitude: site.longitude }}
+              key={`${site.id}${site.line}`}
+              title={site.name}
+              description={site.address}
+            // image={require('../assets/flag-pink.png')}
+            >
+              <TouchableWithoutFeedback
+                onPress={() => this.startAnimation(site.id)}
+              >
+
+                {this.renderTouchedSite(site.id)}
+
+              </TouchableWithoutFeedback>
+
+            </MapView.Marker>
+          ))}
+
+        </MapView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-   markerWrap: {
-      alignItems: "center",
-      justifyContent: "center",
-   },
-   marker: {
-      width: 20,
-      height: 20,
-      borderRadius: 10,
-      backgroundColor: "rgba(130,4,150, 0.9)",
-   },
-   ring: {
-      backgroundColor: "rgba(130,4,150, 0.3)",
-      position: "absolute",
-      borderWidth: 1,
-      borderColor: "rgba(130,4,150, 0.5)",
-   },
+  markerWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  // marker: {
+  //   width: markerWidth,
+  //   height: markerWidth,
+  //   borderRadius: markerWidth/2,
+  //   opacity: markerOpacity,
+  //   backgroundColor: "rgba(130,4,150, 0.9)",
+  // },
+  ring: {
+    backgroundColor: "rgba(130,4,150, 0.3)",
+    position: "absolute",
+    borderWidth: 1,
+    borderColor: "rgba(130,4,150, 0.5)",
+  },
 });
 
 export default Metro;
