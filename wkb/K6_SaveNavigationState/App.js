fc26052a3519218936a389dfc4ed128ea2adacd0
chsyu <chsyu@ntut.edu.tw>
2020-05-14 14:57:11 +0800

    wkc add Font files

--- wkb/K6_SaveNavigationState/App.js
+++ wkb/K6_SaveNavigationState/App.js
@@ -34,7 +34,7 @@ export default function App(props) {
     loadResourcesAndDataAsync();
   }, []);
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
+  if (!isLoadingComplete) {
     return null;
   } else {
     return (
