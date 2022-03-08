e427e9373d2f8aa1cf863a68de0c24663cb1b8c6
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-03-08 18:34:12 +0800

    wk5: Custom Drawer

---
+++ wk5/E7_StyledDrawer/App.js
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { NativeBaseProvider } from 'native-base';
+
+import Navigation from './src/navigation';
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      {/* <SafeAreaView style={{ flex: 1 }}> */}
+        <NativeBaseProvider>
+          <Navigation />
+        </NativeBaseProvider>
+      {/* </SafeAreaView> */}
+    </SafeAreaProvider>
+
+  );
+}
+
+export default App;
