cd68d2528bfc11fda9074632456c54b510287826
Chi-Shan Yu <chisamyu@seed.net.tw>
2024-04-01 14:27:52 +0800

    wk7:修正LoginScreen merge錯誤

--- wk7/G7_LoginScreen/src/screens/AccountSettingScreen.js
+++ wk7/G7_LoginScreen/src/screens/AccountSettingScreen.js
@@ -1,21 +1,12 @@
 import React, { useState } from 'react';
 import SegmentedControlTab from "react-native-segmented-control-tab";
 import { Box } from "@gluestack-ui/themed";
-<<<<<<<< HEAD:wk7/G7_LoginScreen/src/screens/AccountSettingScreen.js
-========
-import { useSelector } from 'react-redux';
->>>>>>>> refs/remotes/origin/master:wk7/G7_ReduxColorMode/src/screens/AccountSettingScreen.js
 import GeneralAccountScreen from './GeneralAccountScreen';
 import AdvancedAccountScreen from './AdvancedAccountScreen';
-import { selectColorMode } from '../redux/settingsSlice';
 
 export default function TabAccountSettingScreeniewExample() {
    const [selectedIndex, setSelectedIndex] = useState(0);
-<<<<<<<< HEAD:wk7/G7_LoginScreen/src/screens/AccountSettingScreen.js
 
-========
-   const colorMode = useSelector(selectColorMode);
->>>>>>>> refs/remotes/origin/master:wk7/G7_ReduxColorMode/src/screens/AccountSettingScreen.js
    const SegmentedContent = () => {
       return (
          selectedIndex == 0 ?
@@ -25,39 +16,21 @@ export default function TabAccountSettingScreeniewExample() {
    }
 
    return (
-<<<<<<<< HEAD:wk7/G7_LoginScreen/src/screens/AccountSettingScreen.js
       <Box flex={1} >
          <SegmentedControlTab
             values={["General", "Advanced"]}
             tabStyle={{ 
                backgroundColor: "white",
-========
-      <Box flex={1} 
-      bg={colorMode == "light" ? "black" : "black"}
-      >
-         <SegmentedControlTab
-            values={["General", "Advanced"]}
-            tabStyle={{ 
-               backgroundColor: colorMode == "light" ? "white" : "#2d2d2d",
->>>>>>>> refs/remotes/origin/master:wk7/G7_ReduxColorMode/src/screens/AccountSettingScreen.js
                marginTop: 10,
                borderColor: "gray",
             }}
             activeTabStyle={{
-<<<<<<<< HEAD:wk7/G7_LoginScreen/src/screens/AccountSettingScreen.js
                backgroundColor: "gray",
-========
-               backgroundColor: colorMode == "light" ? "gray" : "#666",
->>>>>>>> refs/remotes/origin/master:wk7/G7_ReduxColorMode/src/screens/AccountSettingScreen.js
                marginTop: 10,    
                borderColor: "gray",       
             }}
             firstTabStyle={{ marginLeft: 20 }}
             lastTabStyle={{ marginRight: 20 }}
-<<<<<<<< HEAD:wk7/G7_LoginScreen/src/screens/AccountSettingScreen.js
-========
-
->>>>>>>> refs/remotes/origin/master:wk7/G7_ReduxColorMode/src/screens/AccountSettingScreen.js
             tabTextStyle={{ fontSize: 16, padding: 5, color: "gray"}}
             activeTabTextStyle={{ color: "white" }}
             selectedIndex={selectedIndex}
