aed5033c9a8f883f94c361c56cbfcf050413df20
Chi-Shan Yu <chisamyu@seed.net.tw>
2026-03-12 08:13:09 +0800

    remove old 2026 materials

--- all_projects/5.nativewindCSS/utils/color.js
+++
@@ -1,4 +0,0 @@
-export const colors = {
-  primary: '#E5E5E5',
-  secondary: '#FFFFFF',
-};
--- all_projects/e.searchPage/utils/color.js
+++
@@ -1,5 +0,0 @@
-export const colors = {
-  primary: '#E5E5E5',
-  secondary: '#FFFFFF',
-  fab: '#777',
-};
