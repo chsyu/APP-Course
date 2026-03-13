425aa846cdfd52102832b42200e75b7a80afdba3
Chi-Shan Yu <chisamyu@seed.net.tw>
2026-03-14 07:28:50 +0800

    remove statusbar for android emulator issue

--- wk3/b.StylingStackNavigation/app/_layout.jsx
+++ wk3/b.StylingStackNavigation/app/_layout.jsx
@@ -1,5 +1,4 @@
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 export default function RootLayout() {
@@ -9,7 +8,6 @@ export default function RootLayout() {
         <Stack.Screen name="index" />
         <Stack.Screen name="diary/[id]" />
       </Stack>
-      <StatusBar style="auto" />
     </KeyboardProvider>
   );
 }
